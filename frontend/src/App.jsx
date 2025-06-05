1ebeb39a80facf18cb39f4daa4056907dd4cba10
4rGervais <gervaisniyonshuti@gmail.com>
2025-06-05 11:58:04 +0200

    setting up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@
 function App() {
   return (
     <div className="bg-black ">
-      <h1 className="text-white ">Hello Gervais</h1>
+      <h1 className="text-white text-center">Hello Gervais</h1>
     </div>
   )
 }
